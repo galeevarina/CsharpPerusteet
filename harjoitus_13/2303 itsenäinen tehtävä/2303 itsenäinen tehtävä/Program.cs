4b001b9c6a66e3df2a0ce946e23b78aedce42f24
galeevarina <129209273+galeevarina@users.noreply.github.com>
2023-04-09 22:50:56 +0300

    first commit

---
+++ harjoitus_13/2303 itsenäinen tehtävä/2303 itsenäinen tehtävä/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2303_itsenäinen_tehtävä
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput; // Tallennetaan käyttäjän syöttämä arvo
+            Console.WriteLine();
+            Console.WriteLine("Harjoitus 23.3.");
+            Console.Write("Anna pituus metreinä: ");
+            userInput = Console.ReadLine(); // Käyttäjän syöttämä luku tallessa
+
+            // Muokataan string arvo double dataksi
+            double length = double.Parse(userInput);
+
+            Console.Write("Anna leveys metreinä: ");
+            userInput = Console.ReadLine();
+            double width = double.Parse(userInput);
+
+            double area = length * width;
+            Console.WriteLine($"Alueen pinta-ala: {area}");
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ harjoitus_13/ConsoleApp2_24_03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace projec
+{
+
+    class Program
+    {
+
+        static void Main()
+        {
+            // byte number = 10;
+            // short number = 33000;
+            int number = 33000;
+            // long number = 900;
+
+            // float num = -1000004.534599999f;
+            // double num_2 = 5.324d;
+
+            string word = "Переменная: ";
+            // char symbol = 's';
+
+            // bool isHappy = false;
+
+
+            Console.WriteLine(word + number + ".");
+
+            // Получение данных
+            Console.ReadKey();
+        }
+
+    }
+
+}
