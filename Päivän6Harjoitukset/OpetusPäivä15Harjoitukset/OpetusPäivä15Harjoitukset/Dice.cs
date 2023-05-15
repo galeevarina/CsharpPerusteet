27d8d8c9aff7b16057924693072c340749f0ac9e
galeevarina <rina.galeeva@edu.sakky.fi>
2023-05-15 17:34:15 +0300

    15_5

---
+++ Päivän6Harjoitukset/OpetusPäivä15Harjoitukset/OpetusPäivä15Harjoitukset/Dice.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpetusPäivä15Harjoitukset
+{
+    class Dice
+    {
+        // Properties
+        private Random rng; // Jos luodaan static, tämäm jaetaan kaikkien
+
+        public int MaxValue { get; set; } // Nopassa on lukuja 1 - tämä arvo
+
+
+        public int Value { get; set; } // Nopan arvo, joka on ylöspäin
+
+        // Jokin property, jolla voi tehdä "huijaus" nopan
+
+        // Constructors
+        public Dice(int maxValue)
+        {
+            rng = new Random();
+            MaxValue = maxValue;
+            Roll();
+        }
+
+        public Dice(int maxValue, int value) // Kahden parametrin overload
+        {
+            rng = new Random();
+            MaxValue = maxValue;
+            Value = value;
+            Random random = new Random(4);
+        }
+
+        // Methods
+        // Noppaa "heitetään" ja arvostaan siihen uusi sattunainen arvo
+        public void Roll()
+        {
+            // Random random = new Random();
+            Value = rng.Next(1, MaxValue + 1);
+        }
+
+    }
+
+}
---
+++ Päivän6Harjoitukset/OpetusPäivä15Harjoitukset/OpetusPäivä15Harjoitukset/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpetusPäivä15Harjoitukset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice noppa1 = new Dice(6);
+            Dice noppa2 = new Dice(6, 1);
+
+            noppa1.Roll();
+            
+
+            noppa2.Roll();
+
+
+            Console.WriteLine($"Nopan 1 silmäluku on: {noppa1.Value}");
+            Console.WriteLine($"Nopan 2 silmäluku on: {noppa2.Value}");
+
+            // Tallennetaan erikseen data objektiin
+            // null value on yleinen sovelluksen kaatumissyy
+
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
